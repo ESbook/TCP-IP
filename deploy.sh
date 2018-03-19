c4e260ba544f4c2d928133e2c7d1ca8741e5826c
xu-song <xusong.vip@gmail.com>
2018-03-19 23:05:02 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ echo "book.eson.org" > CNAME
 echo "pushing to github"
 git add -A
 git commit -m "rebuild pages at ${rev}"
-git push -f upstream HEAD:gh-pages
+git push --force --quiet upstream HEAD:gh-pages
