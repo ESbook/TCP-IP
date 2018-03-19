b759ded5e52b1c79667b29c5324ab1bf6843381c
esbook <book.eson.org@gmail.com>
2018-03-19 21:26:17 +0800

    dafadfda

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "hello world"
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "gh-pages" ]
+then 
+    echo "This commit was made against the $TRAVIS_BRANCH and not the gh-pages. No deploy!" 
+#    exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd _book
+
+git init
+git config user.name "esbook"
+git config user.email "book.eson.org@gmail.com"
+git remote add upstream "https://$GH_TOKEN@github.com/ESbook/TCP-IP.git"
+
+echo "book.eson.org" > CNAME
+
+echo "pushing to github"
+git add -A
+git commit -m "rebuild pages at ${rev}"
+git push -f upstream HEAD:gh-pages
